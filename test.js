870e9bb7bc1a4126a430e8086dafec7af82116f4
leejaekwon <nowkwon97@gmail.com>
2023-05-16 11:23:23 +0900

    tsc setting 및 transfile

---
+++ test.js
@@ -0,0 +1,6 @@
+var a = 1;
+var b = 2;
+function c(a, b) {
+    return a + b;
+}
+console.log(c(a, b));
---
+++ test.ts
@@ -0,0 +1,8 @@
+const a = 1;
+const b = 2;
+
+function c(a, b) {
+  return a + b;
+}
+
+console.log(c(a,b));
